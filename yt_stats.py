176066f1bf6ea7ad1018887fd468d0ef21f44ae8
Tanguy Bourlier <tbourlier@LPMON3006448.local>
2025-12-07 18:09:45 +0100

    stats

--- yt_stats.py
+++ yt_stats.py
@@ -73,8 +73,52 @@ def get_videos_ids(playlistId):
     except requests.exceptions.RequestException as e:
         raise e
 
+def batch_list(input_list, batch_size):
+    for i in range(0, len(input_list), batch_size):
+        yield input_list[i:i + batch_size]
+
+def extract_video_data(video_ids):
+    
+    video_stats = []
+    
+    try:
+        for batch in batch_list(video_ids, 50):
+            video_ids_str = ",".join(batch)
+            
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=statistics,snippet&id={video_ids_str}&key={API_KEY}"
+            
+            response = requests.get(url)
+            
+            response.raise_for_status()
+            
+            data = response.json()
+            
+            for item in data['items']:
+                video_id = item["id"]
+                title = item["snippet"]["title"]
+                views = item["statistics"]["viewCount"]
+                likes = item["statistics"].get("likeCount", "N/A")
+                comments = item["statistics"].get("commentCount", "N/A")
+                
+                video_stats.append({
+                    "video_id": video_id,
+                    "title": title,
+                    "views": views,
+                    "likes": likes,
+                    "comments": comments
+                })
+        
+        return video_stats
+    
+    except requests.exceptions.RequestException as e:
+        raise e
 
 if __name__ == "__main__":
     PlaylistId = get_playlistId()
     
-    (get_videos_ids(PlaylistId))
+    video_ids = get_videos_ids(PlaylistId)
+    
+    stats = get_video_stats(video_ids)
+    
+    for stat in stats:
+        print(stat)
